8d211e26c7a90edc73480d8e707c6f0e6867a5d1
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2023-04-17 12:20:13 -0600

    Create Contact me

--- src/constants/index.js
+++ src/constants/index.js
@@ -159,26 +159,26 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
+      "Changed my thought that junior developers can't be a part of the founding team.",
+    name: "Nic Laflamme",
+    designation: "CEO",
+    company: "Origami",
     image: "https://randomuser.me/api/portraits/women/4.jpg",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
+      "Transformed by business from using excel sheets and what not to an outstanding web application.",
+    name: "Chloe Catherine",
+    designation: "Owner",
+    company: "Feel Good Fitness",
     image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
+      "One of the most active students in the class who always things differently and comes up with new ideas.",
+    name: "Ali Moussa",
+    designation: "Instructor",
+    company: "SAIT",
     image: "https://randomuser.me/api/portraits/women/6.jpg",
   },
 ];
